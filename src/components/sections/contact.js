8b3c43448ed54d837f9e3635f1309d52b1f173af
Omkar <omkarsalunkhe3597@gmail.com>
2026-02-12 12:28:35 +0530

    contact section text updates

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,9 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I am actively seeking developer opportunities. 
+        If you have any suitable roles, projects, or would like to collaborate, my inbox is always open. 
+        I look forward to connecting with you.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
